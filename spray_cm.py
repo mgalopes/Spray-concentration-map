1f5bbb15eb2b67c733ffc5e368791e1bea6f7153
Mateus Lopes <garcialopes.mateus@gmail.com>
2023-07-26 12:02:52 -0300

    att

--- spray_cm.py
+++ spray_cm.py
@@ -1,17 +1,17 @@
 import cv2
 
-original=cv2.imread("spray_03037_pos1.png", 1)
-im_gray = cv2.imread("spray_03037_pos1.png", cv2.IMREAD_GRAYSCALE)
+original=cv2.imread('Img000751.tif', 1)
+im_gray = cv2.imread('Img000751.tif', cv2.IMREAD_GRAYSCALE)
 im_color = cv2.applyColorMap(im_gray, cv2.COLORMAP_JET)
-cv2.imshow("Original image", im_gray)
+cv2.imshow('Original image', im_gray)
 cv2.waitKey(0)
-cv2.imshow("edgeDetection2", im_color)
+cv2.imshow('edgeDetection2', im_color)
 cv2.waitKey(0)
 
 #Desenvolvimento da remoção do fundo
 
 # Applying thresholding technique
-_, alpha = cv2.threshold(im_gray, 30, 255, cv2.THRESH_BINARY)
+_, alpha = cv2.threshold(im_gray, 3, 255, cv2.THRESH_BINARY)
   
 # Using cv2.split() to split channels 
 # of coloured image
@@ -26,4 +26,5 @@ rgba = [b, g, r, alpha]
 dst = cv2.merge(rgba, 4)
   
 # Writing and saving to a new image
-cv2.imwrite("gfg_white.png", dst)
+cv2.imwrite('gfg_white.png', dst)
+
